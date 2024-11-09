6cf7415255522e43fc6c8d1aed91dcd0708f3ace
Mukeshwaran Baskaran <mukesh96official@gmail.com>
2024-11-08 21:05:55 -0600

    u

--- api/index.py
+++ api/index.py
@@ -1,22 +1,7 @@
-from flask import Flask, jsonify, render_template
-from flask import send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/api/hello')
-def hello():
-    return jsonify({"message": "Hello from Python!"})
-
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
-
-# This is the entry point for Vercel serverless functions
-def handler(request):
-    """Handle incoming requests and return a response"""
-    with app.test_request_context(request.url):
-        return app.full_dispatch_request()
